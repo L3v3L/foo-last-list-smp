e2b440d1ee40fd8881fce25e11e0983dec974b8d
regorxxx <regorxxx@protonmail.com>
2023-03-02 11:40:45 +0100

    Add files via upload
    Group tags by type. 
    Remove duplicates.
    Sorting by name.
    Covered most standard tags and variations.

--- scripts/last_list_menu.js
+++ scripts/last_list_menu.js
@@ -1,7 +1,7 @@
 'use strict';
-//28/02/23
+//02/03/23
 
-include('menu_xxx.js');
+include('..\\..\\helpers\\menu_xxx.js');
 
 function _lastListMenu(parent) {
 	const menu = new _menu();
@@ -9,19 +9,26 @@ function _lastListMenu(parent) {
 	const sel = plman.ActivePlaylist !== -1 ? fb.GetFocusItem(true) : null;
 	const info = sel ? sel.GetFileInfo() : null;
 	const tags = [
-		{tf: 'ARTIST', val: []},
-		// {tf: 'TITLE', val: []},
-		{tf: 'GENRE', val: []},
-		{tf: 'STYLE', val: []},
-		{tf: 'FOLKSONOMY', val: []}
+		{name: 'Artist(s)',tf: ['ARTIST', 'ALBUMARTIST'], val: [], valSet: new Set(), type: 'ARTIST'},
+		// {name: 'Title', tf: ['TITLE'], val: [], valSet: new Set(), type: 'TITLE'},
+		{name: 'Genre & Style(s)', tf: ['GENRE', 'STYLE', 'ARTIST GENRE LAST.FM', 'ARTIST GENRE ALLMUSIC'], val: [], valSet: new Set(), type: 'TAG'},
+		{name: 'Folsonomy & Date(s)', tf: ['FOLKSONOMY', 'OCCASION','ALBUMOCCASION','DATE'], val: [], valSet: new Set(), type: 'TAG'},
+		{name: 'Mood & Theme(s)', tf: ['MOOD','THEME', 'ALBUMMOOD', 'ALBUM THEME ALLMUSIC', 'ALBUM MOOD ALLMUSIC'], val: [], valSet: new Set(), type: 'TAG'},
 	];
 	if (info) {
 		tags.forEach((tag) => {
-			const idx = info.MetaFind(tag.tf);
-			if (idx !== -1) {
-				let count = info.MetaValueCount(idx);
-				while (count--) {tag.val.push(info.MetaValue(idx, count).trim())};
-			}
+			tag.tf.forEach((tf, i) => {
+				const idx = info.MetaFind(tf);
+				tag.val.push([]);
+				if (idx !== -1) {
+					let count = info.MetaValueCount(idx);
+					while (count--) {
+						const val = info.MetaValue(idx, count).trim();
+						tag.val[i].push(val);
+						tag.valSet.add(val);
+					};
+				}
+			});
 		});
 	}
 	
@@ -29,16 +36,14 @@ function _lastListMenu(parent) {
 	menu.newEntry({entryText: 'sep'});
 	
 	tags.forEach((tag) => {
-		const bSingle = tag.val.length <= 1;
-		const subMenu = bSingle ? menu.getMainMenuName() : menu.newMenu('Current ' + tag.tf.toLowerCase() + '...');
-		if (tag.val.length === 0) {tag.val.push('');}
-		tag.val.forEach((val) => {
-			menu.newEntry({menuName: subMenu, entryText: bSingle ? 'Current ' + tag.tf.toLowerCase() + '\t[' + (val || (sel ? 'no tag' : 'no sel')) + ']' : val, func: () => {
-				let url = '';
-				switch (tag.tf) {
-					case 'STYLE':
-					case 'FOLKSONOMY':
-					case 'GENRE': {
+		const bSingle = tag.valSet.size <= 1;
+		const subMenu = bSingle ? menu.getMainMenuName() : menu.newMenu('Current ' + tag.name + '...');
+		if (tag.valSet.size === 0) {tag.valSet.add('');}
+		[...tag.valSet].sort((a,b) => a.localeCompare(b, 'en', {'sensitivity': 'base'})).forEach((val) => {
+			menu.newEntry({menuName: subMenu, entryText: bSingle ? 'Current ' + tag.name + '\t[' + (val || (sel ? 'no tag' : 'no sel')) + ']' : val, func: () => {
+				let url;
+				switch (tag.type) {
+					case 'TAG': {
 						url = 'https://www.last.fm/tag/' + encodeURIComponent(val.toLowerCase()) + '/tracks';
 						break;
 					}
@@ -47,16 +52,29 @@ function _lastListMenu(parent) {
 						break;
 					}
 					case 'TITLE': {
-						const artist = tags.find((tag) => tag.tf === 'ARTIST').val[0];
-						url = 'https://www.last.fm/music/' + encodeURIComponent(artist) + '/_/' + encodeURIComponent(val.toLowerCase());
+						const artist = tags.find((tag) => tag.type === 'ARTIST').val[0][0] || null;
+						url = artist ? 'https://www.last.fm/music/' + encodeURIComponent(artist) + '/_/' + encodeURIComponent(val.toLowerCase()) : null;
 						break;
 					}
 				}
 				console.log('Searching at: ' + url);
-				parent.run(url);
+				if (url) {parent.run(url);}
 			}, flags: val ? MF_STRING : MF_GRAYED});
 		});
 	});
+	
+	menu.newEntry({entryText: 'sep'});
+	
+	const staticURLS = [
+		// {name: 'Charts', url: 'https://www.last.fm/charts'},
+		{name: 'Top tracks this year', url: 'https://www.last.fm/tag/' + new Date().getFullYear().toString() +'/tracks'},
+		{name: 'Top tracks previous year', url: 'https://www.last.fm/tag/' + (new Date().getFullYear() - 1).toString() +'/tracks'}
+	];
+	staticURLS.forEach((entry) => {
+		menu.newEntry({entryText: entry.name, func: () => {
+			parent.run(entry.url);
+		}});
+	})
 	
 	menu.newEntry({entryText: 'sep'});
 	
